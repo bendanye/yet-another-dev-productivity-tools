293bbd56459ec34e382b04c4548f997fa8176b9f
bendanye <shen_soonng@hotmail.com>
2024-09-07 16:49:40 +0800

    Clear and show input examples button for copy json page

--- pages/copy_json_inside_json.py
+++ pages/copy_json_inside_json.py
@@ -6,8 +6,16 @@ def show_copy_json():
     st.title("Copy Json inside json")
     st.write("This tool is to get json in a json")
 
-    key = st.text_input("key contains json")
-    enter_json = st.text_area("Paste the whole json values", height=200)
+    columns = st.columns([1, 6])
+    with columns[0]:
+        st.button("Clear", on_click=_clear_inputs)
+    with columns[1]:
+        st.button("Show Inputs Example", on_click=_show_example)
+
+    key = st.text_input("key contains json", key="key_input")
+    enter_json = st.text_area(
+        "Paste the whole json values", height=200, key="json_input"
+    )
 
     if key and enter_json:
         if key not in enter_json:
@@ -18,6 +26,19 @@ def show_copy_json():
 
             st.write("Output")
             st.json(selected_json, expanded=2)
+
+
+def _clear_inputs():
+    st.session_state.key_input = ""
+    st.session_state.json_input = ""
+
+
+def _show_example():
+    st.session_state.key_input = "value"
+    st.session_state.json_input = """{
+    "success": true,
+    "value": "{\\"hello\\": \\"testing\\"}"
+}"""
 
 
 if __name__ == "__main__":
