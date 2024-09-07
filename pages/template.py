084f9cb7ef7703726407d27e7b25c7c607121424
bendanye <shen_soonng@hotmail.com>
2024-09-07 16:42:30 +0800

    Clear and show input examples button for template page

--- pages/template.py
+++ pages/template.py
@@ -13,11 +13,17 @@ def show_template():
         "number."
     )
 
+    columns = st.columns([1, 6])
+    with columns[0]:
+        st.button("Clear", on_click=_clear_inputs)
+    with columns[1]:
+        st.button("Show Inputs Example", on_click=_show_example)
+
     col1, col2 = st.columns(2)
 
-    enter_lines = col1.text_area("Lines", height=300)
+    enter_lines = col1.text_area("Lines", height=300, key="lines_input")
 
-    enter_template = col2.text_area("Template", height=200)
+    enter_template = col2.text_area("Template", height=200, key="template_input")
 
     if enter_lines and enter_template:
         increment = -1
@@ -39,6 +45,18 @@ def show_template():
             output += format_line + "\n\n"
 
         st.code(output, language="python")
+
+
+def _clear_inputs():
+    st.session_state.lines_input = ""
+    st.session_state.template_input = ""
+
+
+def _show_example():
+    st.session_state.lines_input = "hello1\nhello2"
+    st.session_state.template_input = (
+        "insert into table (id, col2) values ({INCREMENT_NUM_FROM_1}, {REPLACE});"
+    )
 
 
 def _extract_number(string):
