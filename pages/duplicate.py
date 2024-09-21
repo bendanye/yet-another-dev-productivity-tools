1ba4b4f9f972194049bfb342f40baf3443808c53
bendanye <shen_soonng@hotmail.com>
2024-09-21 10:28:54 +0800

    Change layout

--- pages/duplicate.py
+++ pages/duplicate.py
@@ -14,8 +14,8 @@ def show_duplicate():
         index=None,
     )
 
-    if enter_list:
-        if selected_option == "Whole Line":
+    if selected_option == "Whole Line":
+        if enter_list:
             output = ""
             unique_lines = set()
             for item in enter_list.split("\n"):
@@ -27,23 +27,23 @@ def show_duplicate():
             if output == "":
                 output = "No duplicate"
             col3.code(output, language="python")
-        elif selected_option == "Index":
-            left, right = st.columns(2)
-            split_str = left.text_input("Split Str")
-            index = right.text_input("Index to get")
-            if split_str and index:
-                output = ""
-                unique_lines = set()
-                for item in enter_list.split("\n"):
-                    split_item = item.split(split_str)[int(index)]
-                    if split_item in unique_lines:
-                        output += split_item + "\n"
+    elif selected_option == "Index":
+        left, right = col2.columns(2)
+        split_str = left.text_input("Split Str")
+        index = right.text_input("Index to get")
+        if split_str and index and enter_list:
+            output = ""
+            unique_lines = set()
+            for item in enter_list.split("\n"):
+                split_item = item.split(split_str)[int(index)]
+                if split_item in unique_lines:
+                    output += split_item + "\n"
 
-                    unique_lines.add(split_item)
+                unique_lines.add(split_item)
 
-                if output == "":
-                    output = "No duplicate"
-                col3.code(output, language="python")
+            if output == "":
+                output = "No duplicate"
+            col3.code(output, language="python")
 
 
 if __name__ == "__main__":
