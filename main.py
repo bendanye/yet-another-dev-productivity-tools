774eb362581710ea657657f258d0f2166a01cf95
bendanye <shen_soonng@hotmail.com>
2024-11-26 19:26:19 +0800

    Change page title to dpt instead script name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from streamlit_navigation_bar import st_navbar
 import streamlit as st
 import pages as pg
 
-st.set_page_config(initial_sidebar_state="collapsed")
+st.set_page_config(initial_sidebar_state="collapsed", page_title="DPT")
 
 OPTIONS = {"show_menu": False, "show_sidebar": False, "hide_nav": True}
 
